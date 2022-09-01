90c7fc9e0f905d93c72366c3cf57a6341a36e97e
SusanQuynh <104361016+SusanQuynh@users.noreply.github.com>
2022-09-01 16:09:44 +0700

    Add files via upload

---
+++ fpt aptech/script.js
@@ -0,0 +1,9 @@
+function thongbao(){
+    var traloi= confirm("Bạn có nhu cầu tìm người yêu không")
+    if (traloi){
+        alert("Tự kiếm đi má ai gảnh đâu tìm giùm")
+    }
+    else {
+        alert("Vậy thì ế đi ")
+    }
+}
